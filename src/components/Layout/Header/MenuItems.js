de4085925b7466b5e2d7ea8e73d610258405a78e
Rakhmat <kenzhebaev05@gmail.com>
2023-05-15 10:51:01 +0600

    fixed about link was not working

--- src/components/Layout/Header/MenuItems.js
+++ src/components/Layout/Header/MenuItems.js
@@ -24,7 +24,7 @@ const MenuItems = (props) => {
             : "menu-item-has-children"
         }
       >
-        <Link to="#">О нас</Link>
+        <Link to="/about">О нас</Link>
       </li>
       <li
         className={
