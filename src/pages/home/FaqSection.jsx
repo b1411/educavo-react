169bec2529261ffa0b32dc8b8c920a567f465e06
Rakhmat <kenzhebaev05@gmail.com>
2023-06-02 20:54:27 +0600

    deleted unnecessary files

--- src/pages/home/FaqSection.jsx
+++ src/pages/home/FaqSection.jsx
@@ -1,83 +1,95 @@
-import React, { useState } from 'react';
-import ModalVideo from 'react-modal-video';
+import React, { useState } from "react";
+import ModalVideo from "react-modal-video";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemPanel,
-    AccordionItemButton,
-} from 'react-accessible-accordion';
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion";
+
+import image from "../../assets/img/faq/2.jpeg"
 
 const FaqSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(!isOpen);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const openModal = () => setIsOpen(!isOpen);
-
-    return (
-        <div className="rs-faq-part style1 orange-style pt-100 pb-100 md-pt-70 md-pb-70">
-            <ModalVideo channel='youtube' isOpen={isOpen} videoId='YLN1Argi7ik' onClose={() => { openModal(); }} />
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6 padding-0">
-                        <div className="main-part">
-                            <div className="title mb-40 md-mb-14">
-                                <h2 className="text-part">Frequently Asked Questions</h2>
-                            </div>
-                            <div className="faq-content">
-                                <Accordion className="accordion-style1" preExpanded={'a'}>
-                                    <AccordionItem className="accordion-item" uuid="a">
-                                        <AccordionItemHeading className="card-header">
-                                            <AccordionItemButton className="card-link">
-                                                What are the requirements ?
-                                            </AccordionItemButton>
-                                        </AccordionItemHeading>
-                                        <AccordionItemPanel className="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                                        </AccordionItemPanel>
-                                    </AccordionItem>
-                                    <AccordionItem className="accordion-item" uuid="b">
-                                        <AccordionItemHeading className="card-header">
-                                            <AccordionItemButton className="card-link">
-                                                Does Educavo offer free courses?
-                                            </AccordionItemButton>
-                                        </AccordionItemHeading>
-                                        <AccordionItemPanel className="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                                        </AccordionItemPanel>
-                                    </AccordionItem>
-                                    <AccordionItem className="accordion-item" uuid="c">
-                                        <AccordionItemHeading className="card-header">
-                                            <AccordionItemButton className="card-link">
-                                                What is the transfer application process?
-                                            </AccordionItemButton>
-                                        </AccordionItemHeading>
-                                        <AccordionItemPanel className="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                                        </AccordionItemPanel>
-                                    </AccordionItem>
-                                    <AccordionItem className="accordion-item" uuid="d">
-                                        <AccordionItemHeading className="card-header">
-                                            <AccordionItemButton className="card-link">
-                                                What is distance education?
-                                            </AccordionItemButton>
-                                        </AccordionItemHeading>
-                                        <AccordionItemPanel className="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-                                        </AccordionItemPanel>
-                                    </AccordionItem>
-                                </Accordion>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 padding-0">
-                        <div className="img-part media-icon orange-color">
-                            <a href="#" className="popup-videos" onClick={() => { openModal(); }}><i className="fa fa-play"></i></a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="rs-faq-part style1 orange-style pt-100 pb-100 md-pt-70 md-pb-70">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 padding-0">
+            <div className="main-part">
+              <div className="title mb-40 md-mb-14">
+                <h2 className="text-part">Часто задаваемые вопросы</h2>
+              </div>
+              <div className="faq-content">
+                <Accordion className="accordion-style1" preExpanded={"a"}>
+                  <AccordionItem className="accordion-item" uuid="a">
+                    <AccordionItemHeading className="card-header">
+                      <AccordionItemButton className="card-link">
+                        Какие программы обучения вы предлагаете?
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel className="card-body">
+                      Мы предлагаем программы обучения по следующим
+                      направлениям: <br />
+                      <ul>
+                        <li>Пожарно-технический минимум</li>
+                        <li>Безопасность и охрана труда</li>
+                        <li>Санитарно-эпидемиологическая защищенность</li>
+                        <li>Антитеррористическая защита</li>
+                        <li>Парамедика</li>
+                        <li>Согласительная комиссия</li>
+                      </ul>
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                  <AccordionItem className="accordion-item" uuid="b">
+                    <AccordionItemHeading className="card-header">
+                      <AccordionItemButton className="card-link">
+                        Каковы преимущества обучения на вашем сайте по сравнению
+                        с другими?
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel className="card-body">
+                      Мы предлагаем разнообразные программы обучения,
+                      охватывающие различные области и сферы деятельности.
+                      Обучение доступно онлайн с интерактивными материалами
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                  <AccordionItem className="accordion-item" uuid="c">
+                    <AccordionItemHeading className="card-header">
+                      <AccordionItemButton className="card-link">
+                        Каковы квалификации и опыт преподавателей?
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel className="card-body">
+                      Наши преподаватели обладают высокой квалификацией и
+                      богатым практическим опытом в своей области
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                  <AccordionItem className="accordion-item" uuid="d">
+                    <AccordionItemHeading className="card-header">
+                      <AccordionItemButton className="card-link">
+                        Какой формат обучения предлагается: онлайн, очный или
+                        смешанный?
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel className="card-body">
+                      Мы предлагаем онлайн обучение с интерактивными материалами
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                </Accordion>
+              </div>
             </div>
+          </div>
+          <div className="col-lg-6 padding-0">
+            <img src={image}></img>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default FaqSection;
--- src/pages/home/HomeMain.jsx
+++ src/pages/home/HomeMain.jsx
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
-import SliderDefault from '../../components/Slider/SliderDefault';
-import Categories from './CategoriesSection';
-import Cta from './CtaSection';
-import Testimonial from './TestimonialSection';
-import ScrollToTop from '../../components/Common/ScrollTop';
+import React, { Component } from "react";
+import SliderDefault from "../../components/Slider/SliderDefault";
+import Categories from "./CategoriesSection";
+import Cta from "./CtaSection";
+import Testimonial from "./TestimonialSection";
+import ScrollToTop from "../../components/Common/ScrollTop";
+import FaqSection from "./FaqSection";
 
 class HomeMain extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        {/* SliderDefault-start */}
+        <SliderDefault />
+        {/* SliderDefault-start */}
 
-	render() {
+        {/* Categories-area-start */}
+        <Categories />
+        {/* Categories-area-end */}
 
-		return (
-			<React.Fragment>
-				{/* SliderDefault-start */}
-				<SliderDefault />
-				{/* SliderDefault-start */}
+        {/* Courses-area-start */}
+        {/* <Courses /> */}
+        {/* Courses-area-end */}
 
-				{/* Categories-area-start */}
-				<Categories />
-				{/* Categories-area-end */}
+        {/* Cta-area-start */}
+        <Cta />
+        {/* Cta-area-end */}
 
-				{/* Courses-area-start */}
-				{/* <Courses /> */}
-				{/* Courses-area-end */}
+        {/* FaqSection-area-start */}
+        <FaqSection />
+        {/* FaqSection-area-end */}
 
-				{/* Cta-area-start */}
-				<Cta />
-				{/* Cta-area-end */}
+        {/* testmonial-area-start */}
+        <Testimonial />
+        {/* testmonial-area-end */}
 
-				{/* FaqSection-area-start */}
-				{/* <FaqSection /> */}
-				{/* FaqSection-area-end */}
+        {/* blog-area-start */}
+        {/* <Blog /> */}
+        {/* blog-area-end */}
 
-				{/* testmonial-area-start */}
-				<Testimonial />
-				{/* testmonial-area-end */}
-
-				{/* blog-area-start */}
-				{/* <Blog /> */}
-				{/* blog-area-end */}
-
-				{/* scrolltop-start */}
-				<ScrollToTop
-					scrollClassName="scrollup orange-color"
-				/>
-				{/* scrolltop-end */}
-
-			</React.Fragment>
-		);
-	}
+        {/* scrolltop-start */}
+        <ScrollToTop scrollClassName="scrollup orange-color" />
+        {/* scrolltop-end */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default HomeMain;
